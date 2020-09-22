672172a938047c493eb8888e1abae0c6a5554b0f
nolwazi749 <nolwazizwane7@gmail.com>
2020-09-22 05:58:27 -0400

    function-curry code

--- function-curry.js
+++ function-curry.js
@@ -1,18 +1,9 @@
-function curry(fn) {
-    if (fn.length <= 1) return fn;
-    console.log(curry(56, 7, 8));
-    const generator = (...args) => {
-        if (fn.length === args.length) {
-
-            return fn(...args)
-        } else {
-            return (...args2) => {
-
-                return generator(...args, ...args2)
-            }
+function curry(a) {
+    return (b) => {
+        return (c) => {
+            return a + b + c
         }
     }
-    return generator
 }
-console.log(curry(57, 9, 8))
+console.log(curry(57)(9)(8))
 //this is what i have so far...
