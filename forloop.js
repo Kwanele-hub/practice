55f41506ff83c25826e654bd5c37e101e848f045
nolwazi749 <nolwazizwane7@gmail.com>
2020-10-27 08:00:29 -0400

    for-loop code for calculating average

---
+++ forloop.js
@@ -0,0 +1,9 @@
+ const array =[{name:"kwanele",mark:45},{name:"sipho",mark:55},{name:"zee",mark:75}];
+ 
+ function average(array) {
+    var sum = 0;
+    for(var i = 0; i < array.length;i++){
+        sum += array[i];
+    }
+    return sum / array.length;
+}
