e38678a4a0419b8e02576a03b194dc4d1895ff35
tcg <you@example.comkwanelemkhwanazi688@gmail.com>
2021-04-16 11:47:07 +0200

    arrays

---
+++ arraya.js
@@ -0,0 +1,34 @@
+// var myArray = [4, -5, 0, 2, -67, 8, 10, -34];
+// var positiveNum = [];
+// function positiveNumbers(num){
+//   for( var i = 0; i< num.length; i++){
+//     if (num[i] < 0) {
+//       return myArray.push(positiveNum)
+//     }
+    
+//   }
+//   c onsole.log(newNumber)
+//     if (num >= 0)
+//     return num
+//     else{
+//        return  newNumber <= myArray
+//     } 
+// }
+
+// console.log(positiveNumbers(myArray));
+
+
+// function positiveNumbers(array) {
+  //   return array.filter(num=> num >= 0);
+  // }
+var myArray = [4, -5, 0, 2, -67, 8, 10, -34];
+var positiveArr = [];
+ myArray.forEach(function(num) {
+  if (num < 0) {
+    return num
+  } else {
+    positiveArr.push(num)
+  }
+});
+
+console.log(positiveArr)
